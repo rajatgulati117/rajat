f2ece632a5b5a944bc35d6c34f1ec8b86202f1b4
rajat gulati <rajatgulati117@gmail.com>
2013-05-31 20:40:25 +0530

    commit

---
+++ simple print.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+     main()
+{
+     char gender ,female,male;
+     female='f';
+     male='m';
+     
+    printf("%c \n %c",female,male);
+    
+    getch();
+}          
